178d702517c0694623897f761beba50f1178d0c0
David Hide <d.hide@live.co.uk>
2019-05-27 16:23:06 +0100

    Change prisonersDilemma to a class

--- src/prisonersDilemma.js
+++ src/prisonersDilemma.js
@@ -1,64 +1,53 @@
-import TitForTat from './strategies/TitForTat';
-import RandomChoice from './strategies/RandomChoice';
-import AlwaysBetray from './strategies/AlwaysBetray';
-import AlwaysCooperate from './strategies/AlwaysCooperate';
+export default class PrisonersDilemma {
+  constructor() {
+    this.iterations = 5;
+  }
 
-const ITERATIONS = 5;
-
-/**
-  * player betrays and opponent doesnt: 0
-  * both betray: -2
-  * player stays silent and opponent betrays: -3
-  * @param {bool} playerBetray
-  * @param {bool} opponentBetray
-  */
-function calcResult(playerBetray, opponentBetray) {
-  if (playerBetray) {
-    if (opponentBetray) {
-      return -2;
+  /**
+    * player betrays and opponent doesnt: 0
+    * both betray: -2
+    * player stays silent and opponent betrays: -3
+    * @param {bool} playerBetray
+    * @param {bool} opponentBetray
+    */
+  calcResult(playerBetray, opponentBetray) {
+    if (playerBetray) {
+      if (opponentBetray) {
+        return -2;
+      }
+      // opponent stayed silent
+      return 0;
     }
-    // opponent stayed silent
-    return 0;
+    // player stayed silent
+    if (opponentBetray) {
+      return -3;
+    }
+    // both stayed silent
+    return -1;
   }
-  // player stayed silent
-  if (opponentBetray) {
-    return -3;
+
+  pushHistory(player, opponentBetray) {
+    if (player.opponentHistory) {
+      player.opponentHistory.push(opponentBetray);
+    }
   }
-  // both stayed silent
-  return -1;
+
+  simulateGame(playerOne, playerTwo) {
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
+    for (let i = 0; i < this.iterations; i++) {
+      const playerOneBetray = playerOne.play(i);
+      const playerTwoBetray = playerTwo.play(i);
+      console.log(`${playerOne.name} betray: ${playerOneBetray}`);
+      console.log(`${playerTwo.name} betray: ${playerTwoBetray}`);
+      console.log("...");
+
+      playerOne.score += this.calcResult(playerOneBetray, playerTwoBetray);
+      playerTwo.score += this.calcResult(playerTwoBetray, playerOneBetray);
+
+      this.pushHistory(playerOne, playerTwoBetray);
+      this.pushHistory(playerTwo, playerOneBetray);
+    }
+  }
 }
-
-function pushHistory(player, opponentBetray) {
-  if (player.opponentHistory) {
-    player.opponentHistory.push(opponentBetray);
-  }
-}
-
-function simulateGame(playerOne, playerTwo) {
-  let playerOneScore = 0;
-  let playerTwoScore = 0;
-
-  for (let i = 0; i < ITERATIONS; i++) {
-    const playerOneBetray = playerOne.play(i);
-    const playerTwoBetray = playerTwo.play(i);
-		console.log(`${playerOne.name} betray: ${playerOneBetray}`);
-		console.log(`${playerTwo.name} betray: ${playerTwoBetray}`);
-    console.log("...");
-
-    playerOneScore += calcResult(playerOneBetray, playerTwoBetray);
-    playerTwoScore += calcResult(playerTwoBetray, playerOneBetray);
-
-    pushHistory(playerOne, playerTwoBetray);
-    pushHistory(playerTwo, playerOneBetray);
-  }
-
-  console.log(`${playerOne.name} score: ${playerOneScore}`);
-  console.log(`${playerTwo.name} score: ${playerTwoScore}`);
-}
-
-const titForTat = new TitForTat();
-const randomChoice = new RandomChoice();
-const alwaysBetray = new AlwaysBetray();
-const alwaysCooperate = new AlwaysCooperate();
-
-simulateGame(titForTat, randomChoice);
